9a578d37a19ad91b17df22f3bb72b00989242434
ambili <ambilipakhd@gmail.com>
2022-11-22 23:50:12 +0530

    updated

--- src/Cube/Cube.jsx
+++ src/Cube/Cube.jsx
@@ -3,47 +3,36 @@ import './Cube.css'
 import Cube from 'react-3d-cube';
 
 function LogoCube() {
+
+    const handleRotation=(obj,sub)=>{
+   console.log("oo",obj,sub);
+    }
     
     return (
       <div>
             
           
         <center>
-            {/* <h2>no children</h2> */}
-            {/* <div
-            style={{
-                width: 300,
-                height: 300
-            }}
-            >
-            <Cube size={300} index="front" />
-            </div>
-            <h2>set children</h2> */}
+            
             <div
             style={{
                 width: 300,
                 height: 300
             }}
             >
-            <Cube size={300} index="front" >
-                <div style={{backgroundColor:"red",opacity:"0.8"}} className='frontone'>
-                    <div style={{backgroundColor:"red",width: '280px', height: '300px'}}></div>
+            <Cube size={300} index="front" className >
+                <div className='first'  onClick={handleRotation(this)}>
                     </div>
 
-                <div style={{backgroundColor:"green",opacity:"0.8"}}>
-                    <div style={{backgroundColor:"green",width: '280px', height: '300px'}}></div>
+                <div  className='second' >
                     </div>
-                <div style={{backgroundColor:"blue",opacity:"0.8"}}>
-                    <div style={{backgroundColor:"blue",width: '280px', height: '300px'}} ></div>
+                <div  className='third'>
                     </div>
-                <div style={{backgroundColor:"yellow",opacity:"0.8"}}>
-                    <div style={{backgroundColor:"yellow",width: '280px', height: '300px'}} ></div>
+                <div  className='fourth'>
                     </div>
-                <div style={{backgroundColor:"orange",opacity:"0.8"}}>
-                    <div style={{backgroundColor:"orange",width: '280px', height: '300px'}} ></div>
+                <div  className='fifth'>
                 </div>
-                <div style={{backgroundColor:"violet",opacity:"0.8"}}>
-                    <div style={{backgroundColor:"violet",width: '280px', height: '300px'}}></div>
+                <div  className='sixth'>
                     </div>
             </Cube>
             </div>
